bcc981f3b55933a639c0b881233c6a1b4dabed59
yurishe0 <yurishe00@gmail.com>
2024-06-24 14:31:14 +0200

    feat: implement and style homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,18 @@
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
+
 function Home() {
     return (
-        <h1>Hello World</h1>
+        <div className={styles.home}>
+            <div className={styles.motto}>
+                <h1>Welcome to UltraShop!</h1>
+                <p>The best products, for the best customers.</p>
+            </div>
+            <p className={styles.about}>Our mission is simple: to provide a seamless shopping experience with exceptional customer service. Whether you’re looking for the latest electronics, trendy fashion, home essentials, or rare collectibles, we’ve got you covered. Our extensive inventory ensures that you can find exactly what you’re looking for, all in one place.</p>
+            <Link to="/shop">
+            <button className={styles.shopButton}>Shop now!</button>
+            </Link>
+        </div>
     )
 }
 
