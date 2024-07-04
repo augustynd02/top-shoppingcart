7f9c4c1f217e7dabdbec6abb425a55ccdc621455
yurishe0 <yurishe00@gmail.com>
2024-07-04 19:02:26 +0200

    feat: change the header based on cart content

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,7 +6,7 @@ function Cart() {
     const [cart, setCart] = useOutletContext();
     return (
         <div className={styles.cartContainer}>
-            <h2>Your shopping cart:</h2>
+            {cart.length > 0 ? <h2>Your Shopping Cart:</h2> : <h2>Your cart is empty!</h2>}
             <div className={styles.cart}>
                 {cart.map(item => {
                     return <CartItem item={item} cart={cart} setCart={setCart} key={item.id} />
