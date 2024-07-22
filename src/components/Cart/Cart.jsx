689133d7e8edd2014fdd984463bf3d934285ca56
yurishe0 <yurishe00@gmail.com>
2024-07-22 12:36:57 +0200

    feat: calculate the total price of the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import styles from './Cart.module.css';
 
 function Cart() {
     const [cart, setCart] = useOutletContext();
+    const total = cart.map(item => item.price * item.quantity).reduce((sum, cur) => sum + cur, 0);
     return (
         <div className={styles.cartContainer}>
             {cart.length > 0 ? <h2>Your Shopping Cart:</h2> : <h2>Your cart is empty!</h2>}
@@ -11,6 +12,9 @@ function Cart() {
                 {cart.map(item => {
                     return <CartItem item={item} cart={cart} setCart={setCart} key={item.id} />
                 })}
+            </div>
+            <div className={styles.checkout}>
+                <h2>Total: {total}$</h2>
             </div>
         </div>
     )
